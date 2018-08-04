68b2091dc2f9d00e03771767bffd35e70f014e55
Ethan Quan Tong <tongdamtrungquan@gmail.com>
2018-08-04 18:49:04 -0400

    psycopg-binary and Debug
    Former-commit-id: 51e41399653b2aa28c110c0a5fbd0c91f631caaf

--- travel_safety/settings.py
+++ travel_safety/settings.py
@@ -28,7 +28,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = 'yzulcpjb7!d3q0p#=)l$blo&4(*ymu+0i&7#hx12c3j5kawl%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['travel-safety.herokuapp.com', '127.0.0.1']
 
